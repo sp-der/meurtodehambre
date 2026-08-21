f0ef980264ebf58408bdb78dd26f9aa4aebf629e
sp-der <98449997+sp-der@users.noreply.github.com>
2026-08-20 17:36:17 -0700

    Fix root hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
