da0476ffc5ee97ce25d77a780b74070cb1759dd9
sp-der <98449997+sp-der@users.noreply.github.com>
2026-08-20 20:08:56 -0700

    Load high resolution and map styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import './brand.css';
+import './quality.css';
 
 export const metadata: Metadata = {
   title: 'Muerto De Hambre Grill',
